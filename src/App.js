8b2019f91b6836a748ddd52b644d3552964f6728
JackBecknell <52moosefit@gmail.com>
2022-04-04 12:28:44 -0400

    Reinstalled react + Finished functionality for EntriesChartTracker + Finished functionality for addEntry

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import { useState } from "react";
 import DisplayEntries from "./components/displayEntries/displayEntries";
-import AddEntryForm from "./components/displayEntries/addEntry/addEntryForm";
+import AddEntryForm from "./components/addEntry/addEntryForm";
+import EntriesChartTracker from "./components/EntriesChartTracker/EntriesChartTracker";
 
 function App() {
   
   const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'},{weight: 180, date: '11-24-2021'},{weight: 179, date: '11-25-2021'}])
 
   function addNewEntry (entry) {
-    let tempEntries = [...entries, entdry];
+    let tempEntries = [...entries, entry];
     setEntries(tempEntries)
   }
   return (
     <div>
       <DisplayEntries parentEntries = {entries}/>
-      <AddEntryForm addNewEntryProperty={addNewEntry}/>
+      <AddEntryForm addNewEntryProperty = {addNewEntry}/>
+      <EntriesChartTracker parentEntries = {entries}/>
     </div>
   );
 }
